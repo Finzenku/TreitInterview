34fe2ddd7a466addeddd0d3cf4cb2f303b9e8322
Vi Ness <kevfol@hotmail.com>
2023-06-26 13:23:48 -1000

    Update Program.cs Uses smaller chunks and specifies that the output is the distinct value count

--- TreitFileParser/Program.cs
+++ TreitFileParser/Program.cs
@@ -4,7 +4,7 @@ const string inputFolder = @"M:\TreitFileGenerator\";
 const string outputFolder = inputFolder;
 const int bin1Seed = 5185;
 const int bin2Seed = 518;
-const int SplitSizeInMB = 1024;
+const int SplitSizeInMB = 50;
 
 
 string bin1Path = $"{inputFolder}{bin1Seed}.bin";
@@ -14,4 +14,4 @@ bin1Path = FileSorter<int>.SortReallyBigFile(bin1Path, SplitSizeInMB);
 bin2Path = FileSorter<int>.SortReallyBigFile(bin2Path, SplitSizeInMB);
 
 int[] distinctValues = FileComparer<int>.GetDistinctValues(bin1Path, bin2Path);
-Console.WriteLine(distinctValues.Length);
+Console.WriteLine($"Distinct Value Count: {distinctValues.Length}");
